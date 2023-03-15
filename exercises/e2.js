83d1534397f46d8d7fada519474ed5376e7888f0
Carl Boquiren <carlboquiren@gmail.com>
2023-03-15 16:12:23 -0400

    Complete Exercise 2

--- exercises/e2.js
+++ exercises/e2.js
@@ -12,12 +12,21 @@
 
 // Your function code starts here...
 
+function rectArea (l, w) {
+  return l * w;
+}
+
 
 // Use the function you created above to calculate the areas of 3 rooms
 // Assign the results to room1, room2, and room3 variables
 // Data for calculation: room1: 4 by 6, room2: 13 by 3, room3: 4 by 8
 
 // Your code goes here:
+
+var room1 = rectArea(4, 6);
+var room2 = rectArea(13, 3);
+var room3 = rectArea(4, 8);
+
 
 
 
@@ -33,12 +42,16 @@
 
 // Your code goes here...
 
+function totalRoomsArea (a, b, c) {
+  return a + b + c;
+}
+
 
 
 // Make the function call in the parentheses of the console.log statement.
 // Use room1, room2, room3 variables as arguments for the function call
 const result = function () {
-    console.log(/* replace this comment with a function call here... */);
+    console.log(totalRoomsArea(room1, room2, room3));
 }
 
 
